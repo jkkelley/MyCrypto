c8e63598980dea989c817c3f92f575293b1ee258
James Kelley <jkkelley86@gmail.com>
2021-06-27 11:42:46 -0500

    created a profile reducer to hold form inputs

---
+++ src/redux/reducers/form.submission.reducer.js
@@ -0,0 +1,19 @@
+export const formSubmission = (state = [], action) => {
+  // A check to see which action.type we received
+  switch (action.type) {
+    case "SET_FIRST_NAME_CREATE_PROFILE":
+      return { ...state, first: action.payload };
+    case "SET_PROFILE_IMAGE_CREATE_PROFILE":
+      return { ...state, image: action.payload };
+    case "SET_LAST_NAME_CREATE_PROFILE":
+      return { ...state, last: action.payload };
+    case "SET_NICKNAME_CREATE_PROFILE":
+      return { ...state, nickname: action.payload };
+    case "SET_EMAIL_CREATE_PROFILE":
+      return { ...state, email: action.payload };
+    case "SET_PHONE_NUMBER_CREATE_PROFILE":
+      return { ...state, phone_number: action.payload };
+    default:
+      return state;
+  }
+};
