184cf6f2faae6298e7dfdc8f95268a1389bae9c4
James Kelley <jkkelley86@gmail.com>
2021-06-28 07:58:56 -0500

    getCreateProfile ready to talk to server.

--- src/redux/sagas/form.submission.saga.js
+++ src/redux/sagas/form.submission.saga.js
@@ -1,19 +1,37 @@
 import { put, takeLatest, takeEvery } from "redux-saga/effects";
 import axios from "axios";
 
-function* postCreateForm(action) {
+// POST request to server
+function* postCreateProfile(action) {
   console.log(action.user.id);
   try {
     yield axios.post(`/api/createProfile`, action.payload);
   } catch (error) {
-    console.log(`We had an postCreateForm error...`, error);
+    console.log(
+      `We had an postCreateForm error, /api/createProfile ... `,
+      error
+    );
   }
 }
 
-function* createFormSaga() {
-  // Post Route to server.
-  yield takeLatest("POST_CREATE_PROFILE", postCreateForm);
+// GET Request to server
+function* getCreateProfile(action) {
+  try {
+    const response = yield axios.get("/api/createProfile");
+    console.log(`Server Says... ${response}`);
+    // Set profile to reducer
+    yield put({ type: "SET_PROFILE_INFO", payload: response.data });
+  } catch (error) {
+    console.log(`We had a GET error, /api/createProfile ... `, error);
+  }
+}
+
+function* createProfileSaga() {
+  // Saga to watch GET Route to server.
+  yield takeLatest("GET_CREATE_PROFILE", getCreateProfile);
+  // Saga to watch POST Route to server.
+  yield takeLatest("POST_CREATE_PROFILE", postCreateProfile);
 }
 
 // Export listeners to the root
-export default createFormSaga;
+export default createProfileSaga;
