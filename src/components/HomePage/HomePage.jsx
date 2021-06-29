84c5255600e6caab1494601dc9262b849e430766
James Kelley <jkkelley86@gmail.com>
2021-06-28 20:21:27 -0500

    basic skeleton for  homePage complete.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,33 @@
-function HomePage () {
-    return (
-        <>
-            <p>Home Page</p>
-        </>
-    )
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams, Redirect } from "react-router-dom";
+
+// Material-ui Imports
+import Button from "@material-ui/core/Button";
+
+// SweetAlert2
+import Swal from "sweetalert2";
+
+function HomePage() {
+  const profileData = useSelector((store) => store.profileData);
+  return (
+    <>
+      <div>
+        <h3>Welcome</h3>
+        <p>{profileData[0]?.users_nickname}</p>
+      </div>
+      <br></br>
+      <div>
+        <h3>Balance</h3>
+        <p>
+          {Number(profileData[0]?.account_balance).toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
